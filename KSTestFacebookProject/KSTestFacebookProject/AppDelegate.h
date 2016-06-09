7c2e6e6fe5c4fece4eb08a1f9f05f570b1e8cee3
KulikovS <sexadmiral@gmail.com>
2016-06-09 18:17:45 +0300

    TestFacebookProject was created

---
+++ KSTestFacebookProject/KSTestFacebookProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KSTestFacebookProject
+//
+//  Created by KulikovS on 09.06.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KSTestFacebookProject/KSTestFacebookProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSTestFacebookProject
+//
+//  Created by KulikovS on 09.06.16.
+//  Copyright © 2016 KulikovS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
